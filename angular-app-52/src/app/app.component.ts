6617931dafe478d0fd7014a12b963eeefdebc02f
vasudev <code.vasu@gmail.com>
2025-06-26 13:51:41 +0530

    new angular project added

--- angular-app-52/src/app/app.component.ts
+++ angular-app-52/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
